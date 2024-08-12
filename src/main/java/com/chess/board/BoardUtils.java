ffc9e0a777b37c4193789d97e1bdac49da2c307e
Aziz Gharbi <gharbi.aziz03@gmail.com>
2024-08-12 10:21:18 +0100

    Working on Move Class

--- src/main/java/com/chess/board/BoardUtils.java
+++ src/main/java/com/chess/board/BoardUtils.java
@@ -1,10 +1,20 @@
 package com.chess.board;
 
+import java.util.ArrayList;
+
 public class BoardUtils {
-    public static final boolean[] SECOND_COLUMN = null;
-    public static final boolean[] FIRST_COLUMN = null;
-    public static final boolean[] SEVENTH_COLUMN = null;
-    public static final boolean[] EIGHTH_COLUMN = null;
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+    private static boolean[] initColumn(int columnNumber){
+        final boolean[] column = new boolean[64];
+        do{
+            column[columnNumber] = true;
+            columnNumber += 8;
+        }while(columnNumber < 64);
+        return column;
+    }
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
     private BoardUtils(){
         throw new RuntimeException("Nope!");
